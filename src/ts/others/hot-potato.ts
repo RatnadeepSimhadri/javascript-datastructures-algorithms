1cf0902404ae1dc0df3bf229b88fe004ed867c1e
Ratnadeep Simhadri <rsimhadri@LM-SJN-21005917.paypal.com>
2019-01-25 19:37:28 -0800

    my hotpotato implementation

--- src/ts/others/hot-potato.ts
+++ src/ts/others/hot-potato.ts
@@ -1,22 +1,22 @@
+/**
+  My Hot Potato Game : Via a Circular Queue. You take from Beginning and add to the End.
+ */
 import Queue from '../data-structures/queue';
-
-export function hotPotato(elementsList: any[], num: number) {
-  const queue = new Queue();
-  const elimitatedList = [];
-
-  for (let i = 0; i < elementsList.length; i++) {
-    queue.enqueue(elementsList[i]);
-  }
-
+export function hotPotato(names: Array<string>, number: number) {
+  const queue = new Queue<string>();
+  const eliminatedList: Array<string> = [];
+  names.forEach(name => queue.enqueue(name));  // Places all the Players in a Queue
+  // Create a Circular Queue where you take a Player from the Top and Add him to the Back. its like passing
+  // hot potato over until a certain condition is met . This condition here is i being equal to the Number
+  // In which case you remove the person from the Queue
   while (queue.size() > 1) {
-    for (let i = 0; i < num; i++) {
-      queue.enqueue(queue.dequeue());
+    for (let i = 0 ; i < number; i++) {
+          queue.enqueue(queue.dequeue());
     }
-    elimitatedList.push(queue.dequeue());
+    eliminatedList.push(queue.dequeue());
   }
-
   return {
-    elimitated: elimitatedList,
+    eliminated: eliminatedList,
     winner: queue.dequeue()
   };
 }
